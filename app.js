f01f2c95e0b7b167b5de002f3e6f519423c21603
Jordan Caumartin <pro.jordanc@gmail.com>
2020-05-21 13:10:36 +0200

    add freeze function

--- app.js
+++ app.js
@@ -67,6 +67,25 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  //Tetromino descend toute les secondes.
+  timerId = setInterval(moveDown, 1000)
+
+  function moveDown(){
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  function freeze(){
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      random = Math.floor(Math.random()*theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
 
 
 
