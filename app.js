8013432ab87131c989b7d8ae948086a1d10b6981
Jordan Caumartin <pro.jordanc@gmail.com>
2020-05-21 14:13:44 +0200

    function gameOver

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   let nextRandom = 0
   let timerId
+  let score = 0
 
   //Les tetrominos
   const lTetromino = [
@@ -70,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //Tetromino descend toute les secondes.
-  timerId = setInterval(moveDown, 1000)
+  //timerId = setInterval(moveDown, 1000)
 
   function controls(e){
     if(e.keyCode === 37){
@@ -101,6 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4
       draw()
       displayShape()
+      addScore()
+      gameOver()
     }
   }
 
@@ -182,6 +185,31 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 
+  function addScore(){
+    for (let i = 0; i < 199; i += width){
+      const row = [ i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+      if (row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10
+        scoreDisplay.innerHTML = score
+        row.forEach( index => {
+          squares[index].classList.remove('taken')
+          squares[index].classList.remove('tetromino')
+        })
+        const squaresRemoved = squares.splice(i, width)
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
+      }
+    }
+  }
+
+  function gameOver(){
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+      scoreDisplay.innerHTML = "end"
+      clearInterval(timerId)
+    }
+
+  }
 
 
 
